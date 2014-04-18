47cba35f25d31f9e99f6aff80c70e634dcd9c32b
ebrehault <ebrehault@gmail.com>
2014-04-18 15:16:45 +0200

    shop program layout

--- src/chanel.skin/chanel/skin/static/js/theme.js
+++ src/chanel.skin/chanel/skin/static/js/theme.js
@@ -1,18 +1,22 @@
-function equalHeight(group) {
-	var tallest = 0;
-	group.each(function() {
-		var thisHeight = $(this).height();
-		if(thisHeight > tallest) {
-			tallest = thisHeight;
-		}
-	});
-	group.height(tallest);
-}
-
-function initializeuebthesesenbretagne(){
-    /*
-    $('body.section-homepage #portal-column-one').toggleClass('span3').toggleClass('');
-    $('body.section-homepage #portal-column-two').toggleClass('span3').toggleClass('span4');
-    */
-}
-jQuery(initializeuebthesesenbretagne);
+chanel = {};
+chanel.planning = {};
+chanel.planning.enable_choices = function() {
+    $('.planning-table input[type="radio"]').change(function() {
+        if(this.value=='Y') {
+            $(this).parents('.movie-cell').removeClass('no');
+            $(this).parents('.movie-cell').addClass('yes');
+            $(this).parents('.movie-cell').find('input[type="date"]').removeAttr('disabled');
+        } else if(this.value=='N') {
+            $(this).parents('.movie-cell').removeClass('yes');
+            $(this).parents('.movie-cell').addClass('no');
+            $(this).parents('.movie-cell').find('input[type="date"]').attr('disabled', 'disabled');
+        } else {
+            $(this).parents('.movie-cell').removeClass('yes');
+            $(this).parents('.movie-cell').removeClass('no');
+            $(this).parents('.movie-cell').find('input[type="date"]').attr('disabled', 'disabled');
+        }
+    });
+};
+$(document).ready(function() {
+    chanel.planning.enable_choices();
+});
