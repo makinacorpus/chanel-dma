055dc88fb8cd6b81dd69403c908a22e912f2aa6a
Julien Marinescu <jma@makina-corpus.com>
2014-04-25 09:28:54 +0000

    js on submit event for frmShopProgram

--- src/chanel.skin/chanel/skin/static/js/theme.js
+++ src/chanel.skin/chanel/skin/static/js/theme.js
@@ -1,5 +1,6 @@
 chanel = {};
 chanel.planning = {};
+
 chanel.planning.enable_choices = function() {
     $('.planning-table input[type="radio"]').change(function() {
         if(this.value=='Y') {
@@ -17,6 +18,60 @@ chanel.planning.enable_choices = function() {
         }
     });
 };
+
+chanel.planning.event_onsave = function() {
+    $('form[name="frmShopProgram"] input[name="save"]').click(function(event){
+        event.preventDefault();
+
+        var results = {}
+
+        var $displays = $(".movies-row .movies-column");
+
+        /** loop on displays **/
+        for (var i = 0; i < $displays.length; i++) {
+            var $display = $displays.eq(i);
+            var $movies = $(".movie-cell", $display);
+
+            var display_id = $display.attr("rel");
+            results[display_id] = {};
+            /** loop on movies for a specific display **/
+            for (var j = 0; j < $movies.length; j++) {
+                var $movie = $movies.eq(j);
+                var $dates = $("[type=date]", $movie);
+
+                var status = $("[type=radio]:checked", $movie).val() || "";
+                var date1 = $dates.eq(0).val();
+                var date2 = $dates.eq(1).val();
+
+                results[display_id][$movie.attr("rel")] = { status: status, start_date: date1, end_date: date2 };
+            }
+        };
+        $("#planning_data").val(JSON.stringify(results));
+        console.log(results);
+        $form = $(this).parents("form:first");
+        //form.attr("method", "POST").attr("action", "saveDocument");
+        $form.submit();
+    });
+};
+
+chanel.initDatepicker = function() {
+    $('input[type=date]').each(function() {
+        var $input = $(this);
+        $input.datepicker({
+            minDate: $input.attr('min'),
+            maxDate: $input.attr('max'),
+            dateFormat: 'yy-mm-dd'
+        });
+    });
+};
+
 $(document).ready(function() {
+
     chanel.planning.enable_choices();
+    chanel.planning.event_onsave();
+
+    if(!Modernizr.inputtypes.date){
+        chanel.initDatepicker();
+    };
+
 });
